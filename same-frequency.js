49a3c7f0c45ab5816d02bbf866d10641f45d8976
Coco-ha <135057744+Coco-ha@users.noreply.github.com>
2023-07-07 18:31:01 -0500

    worked on two other probs

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,20 @@
+"use strict";
 // add whatever parameters you deem necessary & write docstring
-function sameFrequency() {
+function sameFrequency(num1,num2) {
+  const str1 = num1.toString().split('');
+  const str2 = num1.toString().split('');
+  const numToCount ={}
+  if(str1.length !== str2.length) return false;
+
+  for(let num of str1){
+    numToCount[num] = (numToCount[num] || 0 ) + 1;
+  }
+
+  for(let num of str2){
+    if(num in numToCount){
+      numToCount[num]--;
+    }
+  }
+
+  return Object.values(numToCount).every(char => char === 0)
 }
--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,14 @@
+"use strict";
 // add whatever parameters you deem necessary & write docstring
-function twoArrayObject() {
+function twoArrayObject(keys,values) {
+  const letterToNum = {}
+  let i=0
+
+
+  while(i<keys.length){
+    letterToNum[keys[i]] = values[i] || null;
+    i++;
+  }
+  return letterToNum
+
 }
