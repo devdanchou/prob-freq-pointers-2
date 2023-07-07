ebb77e719d7fc8d8947ed3ee82eb41105e22bf2c
Daniel Chou <daniel.cw.chou@gmail.com>
2023-07-07 18:59:53 -0400

    worked on first two problems

--- average-pair.js
+++ average-pair.js
@@ -1,4 +1,25 @@
+"use strict";
+
 // add whatever parameters you deem necessary & write docstring
-function averagePair() {
+//** this function takes in an array of nums and a target number
+/* utilize two pointers to find if there is a pair matches that target
+returns a boolean
+*/
+function averagePair(nums, targetAvg) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    const avg = (nums[left] + nums[right]) / 2;
+    if (avg == targetAvg) {
+      return true;
+    } else if (avg > targetAvg) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+
+  return false;
 }
 
--- can-construct-word.js
+++ can-construct-word.js
@@ -1,3 +1,36 @@
+"use strict";
+
 // add whatever parameters you deem necessary & write doc comment
-function canConstructWord() {
+//** the function takes in a word and letters
+/* makes a frequency counter and subtracts the count of the object
+returns a boolean
+*/
+function canConstructWord(word, letters) {
+
+  if (word.length > letters.length) return false;
+
+  const freqCounter = {};
+
+  for (let char of letters) {
+    freqCounter[char] = (freqCounter[char] || 0) + 1;
+  }
+
+  for (let char of word) {
+    if (!freqCounter[char]) {
+      return false;
+    } else {
+      freqCounter[char]--;
+    }
+  }
+
+  return true;
 }
+
+console.log(canConstructWord('aa', 'abc'));
+  // false -- can't build "aa" with only 1 "a"
+
+console.log(canConstructWord('abc', 'dcba'));
+  // true -- can build "abc" with letters "abcd"
+
+console.log(canConstructWord('aabb', 'bcabad'));
+  // true -- can build "aabbcc" with those letters
